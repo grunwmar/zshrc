2cd0bb7c03be652d08065837949d077f491420cd
Martin Grünwald-Schreiber <34436146+grunwmar@users.noreply.github.com>
2026-01-17 19:05:33 +0100

    Update prompt-nonerd-simple.sh

--- prompts/prompt-nonerd-simple.sh
+++ prompts/prompt-nonerd-simple.sh
@@ -119,7 +119,7 @@ function precmd() {
   EXITC="%(?..%F{9}-%?-%f$NEWLINE)"
   USERN="%(!.%F{9}%n%f.%n)"
 
-  PROMPT='$NEWLINE$LBRACK%F{13}%n%f]$(colored_path_widget)$(git_prompt_info) $(prompt_sign)'
+  PROMPT='$NEWLINE$LBRACK%F{13}%n%f] $(colored_path_widget)$(git_prompt_info) $(prompt_sign)'
 
   RPROMPT='$(venv_widget)$(sshd_status) $(hostname_clr)'
 }
